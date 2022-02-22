126a9e17a797c18b1db37cc216ae0dfd1ef3129b
maharaniramadani <62225019+maharaniramadani@users.noreply.github.com>
2022-02-22 00:22:41 -0800

    Update praktikum.go
    menghapus slash symbol

--- 3_Introduction to Algorithm and Golang/praktikum.go
+++ 3_Introduction to Algorithm and Golang/praktikum.go
@@ -7,40 +7,40 @@ func main() {
 }
 
 //problem bilangan prima
-// package main
+ package main
 
-// import (
-// 	"fmt"
-// 	"math/big"
-// )
+ import (
+ 	"fmt"
+ 	"math/big"
+ )
 
-// func main(){
-// 	var i int
-// 	fmt.Print("Type a number: ")
-// 	fmt.Scan(&i)
+ func main(){
+ 	var i int
+ 	fmt.Print("Type a number: ")
+ 	fmt.Scan(&i)
 
-// 	if big.NewInt(int64(i)).ProbablyPrime(0) {
-// 		fmt.Println(i,"is prime")
-// 	}else{
-// 		fmt.Println(i,"is not prime")
-// 	}
-// }
+ 	if big.NewInt(int64(i)).ProbablyPrime(0) {
+ 		fmt.Println(i,"is prime")
+ 	}else{
+ 		fmt.Println(i,"is not prime")
+ 	}
+ }
 
 //problem lampu
-// package main
-// import ("fmt")
+package main
+import ("fmt")
 
-// func main (){
-// 	var i int
-// 	fmt.Print("\nMasukkan angka: ")
-// 	fmt.Scan(&i)
+func main (){
+	var i int
+	fmt.Print("\nMasukkan angka: ")
+	fmt.Scan(&i)
 		
-// 	if i<10 {
-// 		if i%2==0 {
-// 			fmt.Print(i,"\nLampu menyala")
-// 		}else{
-// 			fmt.Print(i,"\nlampu mati")
-// 		}
-// 	}
+	if i<10 {
+		if i%2==0 {
+			fmt.Print(i,"\nLampu menyala")
+		}else{
+			fmt.Print(i,"\nlampu mati")
+		}
+	}
 
-// }
+}
