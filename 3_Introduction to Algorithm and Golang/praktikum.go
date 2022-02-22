e1e67b2aae785a9cf4879aac25ac8c8f9c316bc0
maharaniramadani <62225019+maharaniramadani@users.noreply.github.com>
2022-02-22 00:18:32 -0800

    uplod file praktikum

---
+++ 3_Introduction to Algorithm and Golang/praktikum.go
@@ -0,0 +1,46 @@
+//problem hello word
+package main
+import ("fmt")
+
+func main() {
+  fmt.Println("Hello World!")
+}
+
+//problem bilangan prima
+// package main
+
+// import (
+// 	"fmt"
+// 	"math/big"
+// )
+
+// func main(){
+// 	var i int
+// 	fmt.Print("Type a number: ")
+// 	fmt.Scan(&i)
+
+// 	if big.NewInt(int64(i)).ProbablyPrime(0) {
+// 		fmt.Println(i,"is prime")
+// 	}else{
+// 		fmt.Println(i,"is not prime")
+// 	}
+// }
+
+//problem lampu
+// package main
+// import ("fmt")
+
+// func main (){
+// 	var i int
+// 	fmt.Print("\nMasukkan angka: ")
+// 	fmt.Scan(&i)
+		
+// 	if i<10 {
+// 		if i%2==0 {
+// 			fmt.Print(i,"\nLampu menyala")
+// 		}else{
+// 			fmt.Print(i,"\nlampu mati")
+// 		}
+// 	}
+
+// }
