bc82aa1f10d748e259f1988a1a26744d3c3472fb
Maharani Ramadani <62225019+maharaniramadani@users.noreply.github.com>
2022-02-23 22:25:26 -0800

    Delete line with symbol slash

--- 4_Basic Programming/Praktikum/task5.go
+++ 4_Basic Programming/Praktikum/task5.go
@@ -20,27 +20,3 @@ func main() {
   fmt.Println(palindrome("mistar"))
   fmt.Println(palindrome("lion"))
 }
-// package main
-
-// import "fmt"
-
-// func isPalindrome(input string) bool {
-//   for i := 0; i < len(input)/2; i++ {
-//     if input[i] != input[len(input)-i-1] {
-//       return false
-//     }
-// }
-// return true
-// }
-
-// func main() {
-//   var str string
-//   fmt.Scan(&str)
-//   result := isPalindrome(str)
-//   if result == true {
-//     fmt.Println("palindrome")
-//     } else {
-//       fmt.Println("not palindrome")
-//     }
-
-// }
