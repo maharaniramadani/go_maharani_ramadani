78eeed14777c1b0a3db390d27cb62c47c8ffb3b5
maharaniramadani <maharaniramadhani973@gmail.com>
2022-02-23 22:19:31 -0800

    Update task

--- 4_Basic Programming/Praktikum/task8.go
+++ 4_Basic Programming/Praktikum/task8.go
@@ -5,15 +5,15 @@ package main
 import "fmt"
 
 func perkalian(n int) {
-    for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
-            kali := j*i
-            fmt.Scan(&kali)
-            
-        } 
-        
+    var kali int
+    kali=1
+    for i := 1; i <= n; i++ {
+        for j := 1; j <= n; j++ {
+            kali = i*j
+            fmt.Print("",kali,"\t")
+        }   
+    fmt.Println("")
     }
-    
 }
 
 func main(){
