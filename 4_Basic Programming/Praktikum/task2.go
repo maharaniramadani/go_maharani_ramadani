d0ae19343fa62cbff3beecfb6869326b56fef4aa
maharaniramadani <maharaniramadhani973@gmail.com>
2022-02-25 18:41:20 -0800

    update task

--- 4_Basic Programming/Praktikum/task2.go
+++ 4_Basic Programming/Praktikum/task2.go
@@ -17,13 +17,13 @@ func main(){
 
 	// var nilai int = 75
 
-	if nilai > 80 && nilai < 100{
+	if nilai >= 80 && nilai <= 100{
 		fmt.Println("A")
-	}else if nilai < 79 && nilai > 65 {
+	}else if nilai <= 79 && nilai >= 65 {
 		fmt.Println("B")
-	}else if nilai < 64 && nilai > 50 {
+	}else if nilai <= 64 && nilai >= 50 {
 		fmt.Println("C")
-	}else if nilai < 49 && nilai > 35 {
+	}else if nilai <= 49 && nilai >= 35 {
 		fmt.Println("D")
 	}else {
 		fmt.Println("E")
