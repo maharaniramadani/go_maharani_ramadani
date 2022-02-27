09ce4df1e6f2c192cb211989b84d4d0b312b0afb
maharaniramadani <maharaniramadhani973@gmail.com>
2022-02-26 22:00:14 -0800

    Upload task

--- 5_Time Complexity & Space Complexity/Praktikum/Task1.go
+++ 5_Time Complexity & Space Complexity/Praktikum/Task1.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/big"
+	"time"
 )
 
- func prime(num int)bool{
-	for i := 2; i <= int(math.Floor(float64(num) / 2)); i++ {
-        if num%i == 0 {
-            return false
-        }
-    }
-    return num > 1
- }
-
- func main(){
-	 fmt.Println(prime(1000000007))
- }
+func angkaprima (num int64){
+    if big.NewInt(num).ProbablyPrime(0) {
+		fmt.Println(num, " True")
+		time.Sleep(1000 * time.Millisecond)
+	} else {
+		fmt.Println(num, " False")
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
+	
+func main(){
+	start := time.Now()
+	angkaprima(1000000007)
+	fmt.Println("lama waktu:", time.Since(start))
+	angkaprima(13)
+	fmt.Println("lama waktu:", time.Since(start))
+	angkaprima(17)
+	fmt.Println("lama waktu:", time.Since(start))
+    angkaprima(20)
+	fmt.Println("lama waktu:", time.Since(start))
+	angkaprima(35)
+    fmt.Println("lama waktu:", time.Since(start))
+}	
---
+++ 5_Time Complexity & Space Complexity/Praktikum/Task2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+func Pangkat(base, pangkat int) int {
+	var i, result int
+	result = 1
+	for i = 0; i < pangkat; i++ {
+			result *= base
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(Pangkat(2, 3))
+	fmt.Println(Pangkat(5, 3))
+	fmt.Println(Pangkat(10, 2))
+	fmt.Println(Pangkat(2, 5))
+	fmt.Println(Pangkat(7, 3))
+}
