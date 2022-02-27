97a97109e614e561dc4883c2d6fd585a4f36697d
maharaniramadani <maharaniramadhani973@gmail.com>
2022-02-26 23:55:44 -0800

    update task

--- 5_Time Complexity & Space Complexity/Praktikum/Task1.go
+++ 5_Time Complexity & Space Complexity/Praktikum/Task1.go
@@ -2,30 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math/big"
-	"time"
 )
 
-func angkaprima (num int64){
-    if big.NewInt(num).ProbablyPrime(0) {
-		fmt.Println(num, " True")
-		time.Sleep(1000 * time.Millisecond)
-	} else {
-		fmt.Println(num, " False")
-		time.Sleep(1000 * time.Millisecond)
-	}
-}
-	
-func main(){
-	start := time.Now()
-	angkaprima(1000000007)
-	fmt.Println("lama waktu:", time.Since(start))
-	angkaprima(13)
-	fmt.Println("lama waktu:", time.Since(start))
-	angkaprima(17)
-	fmt.Println("lama waktu:", time.Since(start))
-    angkaprima(20)
-	fmt.Println("lama waktu:", time.Since(start))
-	angkaprima(35)
-    fmt.Println("lama waktu:", time.Since(start))
+ func angkaprima(num int)bool{
+	for i := 2; i <= int(num)/2; i++ {
+        if num%i == 0 {
+            return false
+        }
+    }
+    return num > 1
+ }
+
+ func main(){
+	fmt.Println(angkaprima(1000000007))
+	fmt.Println(angkaprima(13))
+	fmt.Println(angkaprima(17))
+    fmt.Println(angkaprima(20))
+	fmt.Println(angkaprima(35))
 }	
