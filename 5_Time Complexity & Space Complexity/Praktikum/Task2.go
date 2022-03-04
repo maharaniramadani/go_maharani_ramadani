8e5546858da695625d0096cf0e13ac56854d45aa
maharaniramadani <maharaniramadhani973@gmail.com>
2022-03-04 10:38:26 -0800

    update

--- 5_Time Complexity & Space Complexity/Praktikum/Task2.go
+++ 5_Time Complexity & Space Complexity/Praktikum/Task2.go
@@ -6,18 +6,21 @@ import (
 )
 
 func Pangkat(base, pangkat int) int {
-	var i, result int
-	result = 1
-	for i = 0; i < pangkat; i++ {
-			result *= base
+	result := 1
+	for pangkat >0{
+		if pangkat%2 == 1 {
+			result*=base
+		}
+		pangkat/=2
+		base*=base
 	}
 	return result
 }
 
 func main() {
 	fmt.Println(Pangkat(2, 10))
-	// fmt.Println(Pangkat(5, 3))
-	// fmt.Println(Pangkat(10, 2))
-	// fmt.Println(Pangkat(2, 5))
-	// fmt.Println(Pangkat(7, 3))
+	fmt.Println(Pangkat(5, 3))
+	fmt.Println(Pangkat(10, 2))
+	fmt.Println(Pangkat(2, 5))
+	fmt.Println(Pangkat(7, 3))
 }
