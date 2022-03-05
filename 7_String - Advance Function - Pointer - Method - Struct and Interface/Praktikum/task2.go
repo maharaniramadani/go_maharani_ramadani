93cb10266298ee8e1df55dad636d8056c70d7eb5
maharaniramadani <maharaniramadhani973@gmail.com>
2022-03-04 21:38:53 -0800

    task2

--- 7_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/task2.go
+++ 7_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/task2.go
@@ -1,22 +1,15 @@
 package main
+
 import "fmt"
 
 func caesar(offset int, input string)string{
-	offset = (offset % 26 + 26) % 26
-    new := make([]byte, len(input))
-    for i := 0; i < len(input); i++ {
-        t := input[i]
-        var a int
-        switch {
-        case 'a' <= t && t <= 'z':
-            a = 'a'
-        default:
-            new[i] = t
-            continue
-		}
-        new[i] = byte(a + ((int(t)-a)+offset)%26)
+    var out string
+    var tem rune
+    for _,i := range input {
+        tem = ((i +rune(offset)  - 97 )%26)+97
+        out +=string(tem)
     }
-    return string(new)
+    return out
 }
 
 
