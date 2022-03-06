4c0dc52b4c5f0c214451d4568edeeb3f53adeef7
maharaniramadani <maharaniramadhani973@gmail.com>
2022-03-05 18:00:45 -0800

    task6

--- 7_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/task6.go
+++ 7_String - Advance Function - Pointer - Method - Struct and Interface/Praktikum/task6.go
@@ -1,5 +1,9 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strings"
+)
 
 type student struct {
 	name string
@@ -14,35 +18,54 @@ type cipher interface {
 
 func (s *student) Encode() string{
 	var nameEncode=""
-TODO:
 
+	encod:=func(offset rune) rune {
+		s := int(offset)+3
+		if s >'z'{
+			return rune(s-26)
+		}else if s <'a'{
+			return rune(s+26)
+		}
+		return rune(s)
+	}
+	nameEncode = strings.Map(encod, s.name)
 	return nameEncode
 }
 
 func (s *student) Decode() string{
 	var nameDecode=""
-TODO:
 
+	dencod:=func(offset rune) rune {
+		s := int(offset)-3
+		if s >'z'{
+			return rune(s-26)
+		}else if s <'a'{
+			return rune(s+26)
+		}
+		return rune(s)
+	}
+	nameDecode = strings.Map(dencod, s.nameEncode)
 	return nameDecode
 }
 
 func main(){
+	var menu int
 	var a =student{}
 	var c cipher=&a
 
-	fmt.Print("[1] Encrypt \n[2] Decrypt \nChoose your menu ? ")
+	fmt.Print("[1]Encrypt \n[2]Decrypt \nchoose your menu: ")
 	fmt.Scan(&menu)
 
-	if menu == 1 {
-		fmt.Print("\nInput Student’s Name : ")
+	if menu == 1{
+		fmt.Print("\ninput student name: ")
 		fmt.Scan(&a.name)
-		fmt.Print("\nEncode of Student’s Name " + a.name + " is : " + c.Encode())
-	} else if menu == 2 {
-		fmt.Print("\nInput Student’s Encode Name : ")
+		fmt.Print("\nencode of student name " +a.name+ " is: " +c.Encode())
+	}else if menu == 2{
+		fmt.Print("\ninput student encode name: ")
 		fmt.Scan(&a.nameEncode)
-		fmt.Print("\nDecode of Student’s Name " + a.nameEncode + " is : " + c.Decode())
-	} else {
-		fmt.Println("Wrong input name menu")
+		fmt.Print("\nDecode of student name " +a.nameEncode+ " is: " +c.Decode())
+	}else{
+		fmt.Println("wrong input name menu")
 	}
 }
 
