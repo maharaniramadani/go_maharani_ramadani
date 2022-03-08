09b13cdfb70cc8f3a02a38d1640025f56a3c7c61
maharaniramadani <maharaniramadhani973@gmail.com>
2022-03-08 11:52:01 -0800

    upload task

---
+++ 8_Recursive - Number Theory - Sorting - Searching/Praktimum/task8.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+type pair struct{
+	name string
+	count int
+}
+
+func appear(item []string)[]pair{
+
+}
+
+func main(){
+	fmt.Println(appear([]string{"js","js","golang","ruby","ruby","js","js"}))
+}
