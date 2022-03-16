a2b22b35d299b1bbd935f2c9f0cf60c3f2a9231f
maharaniramadani <maharaniramadhani973@gmail.com>
2022-03-16 00:51:10 -0700

    update tasak

--- 13_Database/Praktikum/Createdb.sql
+++ 13_Database/Praktikum/Createdb.sql
@@ -54,3 +54,45 @@ MariaDB [alta_online_shop]> drop table shippings;
 Query OK, 0 rows affected (0.017 sec)
 
 -- soal no.7 relation
+
+
+MariaDB [alta_online_shop]> create table payment_method_description (
+    -> id integer not null auto_increment Primary key,
+    -> pm_id integer);
+Query OK, 0 rows affected (0.077 sec)
+
+MariaDB [alta_online_shop]> create table alamat (
+    -> id integer not null auto_increment Primary key,
+    -> id_user integer);
+Query OK, 0 rows affected (0.042 sec)
+
+MariaDB [alta_online_shop]> create table user_payment_method_detail (
+    -> id integer not null auto_increment Primary key,
+    -> id_user integer,
+    -> pm_id integer);
+Query OK, 0 rows affected (0.047 sec)
+
+MariaDB [alta_online_shop]> alter table payment_method_description
+    -> ADD CONSTRAINT FK_PaymentMethod_PaymentMethodDescription
+    -> foreign key (pm_id) References payment_method(id);
+Query OK, 0 rows affected (0.117 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+MariaDB [alta_online_shop]> alter table alamat
+    -> ADD CONSTRAINT FK_alamat_user
+    -> foreign key (id_user) References user(id);
+Query OK, 0 rows affected (0.098 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+
+MariaDB [alta_online_shop]> alter table user_payment_method_detail
+    -> add constraint FK_user_PaymentMethodDetail
+    -> foreign key (id_user) References user(id);
+Query OK, 0 rows affected (0.072 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+MariaDB [alta_online_shop]> alter table user_payment_method_detail
+    -> add constraint FK_PaymentMethod_PaymentMothodDetail
+    -> foreign key (pm_id) References payment_method(id);
+Query OK, 0 rows affected (0.108 sec)
+Records: 0  Duplicates: 0  Warnings: 0
